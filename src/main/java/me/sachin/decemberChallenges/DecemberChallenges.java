63b5dd17456e99bbe78ead9072592d592e7f87c7
Sachin <sachin.kharabanda@aol.com>
2025-11-23 13:49:29 +0000

    first commit

---
+++ src/main/java/me/sachin/decemberChallenges/DecemberChallenges.java
@@ -0,0 +1,17 @@
+package me.sachin.decemberChallenges;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DecemberChallenges extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
